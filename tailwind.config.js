427b3fe97d0332bab7fb9662d127045cc3767605
Nqbral <sim.chevalier@gmail.com>
2024-01-31 21:12:10 +0100

    #1 - Add colors and reorder colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         colors: {
-            white: colors.white,
-            stone: colors.stone,
+            neutral: colors.neutral,
             primary: colors.blue,
+            stone: colors.stone,
+            transparent: colors.transparent,
+            white: colors.white,
         },
         extend: {},
     },
