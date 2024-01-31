91d56b8366e28fec2117e4ded68f64d2930795cd
Nqbral <sim.chevalier@gmail.com>
2024-01-31 21:12:17 +0100

    #1 - Create input form (front only) to login on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,18 +10,36 @@ export default function HomePage() {
                         Construis ton corps, construis des liens
                     </h2>
                 </div>
-                <div className="mt-10 flex flex-col items-center justify-center gap-2 md:mt-0 md:h-screen md:w-1/2">
-                    <h2 className="text-xl font-bold">Inscrivez-vous</h2>
-                    <LinkButton
-                        linkTo={'/signup'}
-                        buttonText={'Créer un compte'}
-                    />
-                    <h3 className="text-lg">Vous avez déjà un compte ?</h3>
-                    <LinkButton
-                        linkTo={'/login'}
-                        buttonText={'Se connecter'}
-                        primary={false}
-                    />
+                <div className="mt-10 flex flex-col items-center justify-center gap-8 md:mt-0 md:h-screen md:w-1/2">
+                    <div className="flex min-w-80 flex-col items-center gap-2 p-8 shadow-md shadow-neutral-500">
+                        <h3 className="text-2xl font-bold">Inscrivez-vous</h3>
+                        <LinkButton
+                            linkTo={'/signup'}
+                            buttonText={'Créer un compte'}
+                        />
+                    </div>
+                    <div className="flex min-w-80 flex-col items-center gap-3 rounded-md p-8 shadow-md shadow-neutral-500">
+                        <h3 className="text-lg font-bold">
+                            Vous avez déjà un compte ?
+                        </h3>
+                        <label htmlFor="username">Nom d&apos;utilisateur</label>
+                        <input
+                            type="text"
+                            name="username"
+                            className="focus:border-primary-500 rounded-md border-2 border-neutral-300 bg-transparent px-1 text-center outline-none transition-colors duration-300"
+                        />
+                        <label htmlFor="password">Mot de passe</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="focus:border-primary-500 rounded-md border-2 border-neutral-300 bg-transparent px-1 text-center outline-none transition-colors duration-300"
+                        />
+                        <LinkButton
+                            linkTo={'/login'}
+                            buttonText={'Se connecter'}
+                            primary={false}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
