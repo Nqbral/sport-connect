b04ea8f0b78a6bd8afe81fec4f0b5a8adbfb3819
Nqbral <sim.chevalier@gmail.com>
2024-03-22 19:58:00 +0100

    [#31] - Logout from navbar and profile bar

--- src/layouts/ProfileBar.jsx
+++ src/layouts/ProfileBar.jsx
@@ -1,4 +1,5 @@
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
@@ -7,28 +8,36 @@ import userLogo from '../assets/user.png';
 import { AuthContext } from '../context/auth.context';
 
 export default function ProfileBar({ linkToBackButton = null }) {
-    const { user } = useContext(AuthContext);
+    const { user, logOutUser } = useContext(AuthContext);
 
     return (
         <div className="fixed top-0 z-10 w-screen bg-primary-700 p-5 py-2 shadow-md shadow-neutral-500">
-            <div className="flex flex-row items-center gap-3">
-                {linkToBackButton !== null ? (
-                    <Link to={linkToBackButton}>
-                        <FontAwesomeIcon
-                            icon={faArrowLeft}
-                            className="text-white"
-                        />
+            <div className="flex flex-row items-center justify-between">
+                <div className="flex flex-row items-center gap-2">
+                    {linkToBackButton !== null ? (
+                        <Link to={linkToBackButton}>
+                            <FontAwesomeIcon
+                                icon={faArrowLeft}
+                                className="text-white"
+                            />
+                        </Link>
+                    ) : (
+                        <></>
+                    )}
+                    <Link
+                        to="/profile"
+                        className="flex flex-row items-center gap-3"
+                    >
+                        <img src={userLogo} className="h-8" alt="user_logo" />
+                        <div className="text-white">@{user.name}</div>
                     </Link>
-                ) : (
-                    <></>
-                )}
-                <Link
-                    to="/profile"
-                    className="flex flex-row items-center gap-3"
+                </div>
+                <button
+                    className="block text-white md:hidden"
+                    onClick={logOutUser}
                 >
-                    <img src={userLogo} className="h-8" alt="user_logo" />
-                    <div className="text-white">@{user.name}</div>
-                </Link>
+                    <FontAwesomeIcon icon={faPowerOff}></FontAwesomeIcon>
+                </button>
             </div>
         </div>
     );
